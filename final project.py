abf7fffdd8d9301ec4a04489e0a35d4900dbab36
huanghe98 <huanghe_98@163.com>
2021-11-14 18:51:31 -0500

    Create final project.py

---
+++ final project.py
@@ -0,0 +1,16 @@
+# To add a new cell, type '#%%'
+# To add a new markdown cell, type '#%% [markdown]'
+
+#%%
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#%%
+df = pd.read_csv("salary.csv")
+
+
+
+#%%
