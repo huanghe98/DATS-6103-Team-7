50d59053bf64276cf95abfe7101293b16dfac3ca
huanghe98 <huanghe_98@163.com>
2021-11-28 22:35:55 -0500

    Update final project.py

--- final project.py
+++ final project.py
@@ -68,7 +68,13 @@ newdf['year'] = newdf.apply(cleanDfTime, colname='timestamp', axis=1)
 newdf.head()
 # Only observations in 2021 and 2020 has no NA in education, race and gender, so year only has two values(2020 and 2021)
 
-#%%
+#%%[markdown]
+## PART 1. introduction and EDA
+
+#%%[markdown]
+## PART 2. model building and evolution for total sallary
+
+#%% 
 
 # quickly check the simple model for total sallary
 from statsmodels.formula.api import ols
@@ -77,15 +83,119 @@ linearmodel1 = ols(formula='totalyearlycompensation ~ yearsofexperience + yearsa
 linearmodel1_fit = linearmodel1.fit()
 print(linearmodel1_fit.summary())
 
-#%%
+
+#%%[markdown]
+## PART 3. model building and evolution for stock
+
+#%% model building and evolution for stock
 
 # quickly check the simple model for stock
 import statsmodels.api as sm
 from statsmodels.formula.api import glm
 
-glm1 = glm(formula='stock ~ totalyearlycompensation +yearsofexperience + yearsatcompany + C(gender) + C(Race) + C(Education) + C(year) ', data=newdf, family=sm.families.Binomial())
+glm1 = glm(formula='stock ~ totalyearlycompensation +yearsofexperience + yearsatcompany + C(gender) + C(Race) + C(Education) + C(year) +C(location)', data=newdf, family=sm.families.Binomial())
 glm1fit = glm1.fit()
 print(glm1fit.summary())
+
+#%%
+
+# build a new model with all significant variables
+glm2 = glm(formula='stock ~ totalyearlycompensation +yearsofexperience + yearsatcompany + C(Race) + C(Education)+ C(year) ', data=newdf, family=sm.families.Binomial())
+glm2fit = glm2.fit()
+print(glm2fit.summary())
+
+#%%
+
+# prediction and confusion matrix
+modelprediction = pd.DataFrame( columns=['prb'], data= glm2fit.predict(newdf))
+modelprediction['glm2'] = np.where(modelprediction['prb'] > 0.5, 1, 0)
+print(pd.crosstab(newdf.stock, modelprediction.glm2,
+rownames=['Actual'], colnames=['Predicted'],
+margins = True))
+
+# Predicted     0      1    All
+# Actual                       
+# 0          2959   3372   6331
+# 1          1725  13535  15260
+# All        4684  16907  21591
+
+print("The total accuaracy of 0.5 cut-off model is",(2959+13535)/21591)
+print("The precision 0.5 cut-off model is",13535/16907)
+print("The recall rate 0.5 cut-off model is",13535/15260)
+
+
+#%% classifiers
+
+# First, spilt total data set to train and test set
+df_x = newdf[['yearsofexperience','yearsatcompany','totalyearlycompensation',]]
+df_y = newdf['stock']
+
+# 4:1 train-test split
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test= train_test_split(df_x, df_y, test_size=0.2, stratify=df_y,random_state=10)
+
+#%% 
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix 
+from sklearn.metrics import classification_report
+
+print("\nReady to continue.")
+
+#%% SVC()
+svc = SVC()
+svc.fit(X_train,y_train)
+print(f'svc train score:  {svc.score(X_train,y_train)}')
+print(f'svc test score:  {svc.score(X_test,y_test)}')
+print(confusion_matrix(y_test, svc.predict(X_test)))
+print(classification_report(y_test, svc.predict(X_test)))
+
+#%% SVC(kernel="linear")
+svc_lr = SVC(kernel="linear")
+svc_lr.fit(X_train,y_train)
+print(f'svc(kernel="linear") train score:  {svc_lr.score(X_train,y_train)}')
+print(f'svc(kernel="linear") test score:  {svc_lr.score(X_test,y_test)}')
+print(confusion_matrix(y_test, svc_lr.predict(X_test)))
+print(classification_report(y_test, svc_lr.predict(X_test)))
+
+#%% LinearSVC()
+linearSVC = LinearSVC()
+linearSVC.fit(X_train,y_train)
+print(f'svc(kernel="linear") train score:  {linearSVC.score(X_train,y_train)}')
+print(f'svc(kernel="linear") test score:  {linearSVC.score(X_test,y_test)}')
+print(confusion_matrix(y_test, linearSVC.predict(X_test)))
+print(classification_report(y_test, linearSVC.predict(X_test)))
+
+#%% LogisticRegression()
+logreg = LogisticRegression()
+logreg.fit(X_train,y_train)
+print(f'lr train score:  {logreg.score(X_train,y_train)}')
+print(f'lr test score:  {logreg.score(X_test,y_test)}')
+print(confusion_matrix(y_test, logreg.predict(X_test)))
+print(classification_report(y_test, logreg.predict(X_test)))
+
+#%% KNN
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(X_train,y_train)
+print(f'KNN train score:  {knn.score(X_train,y_train)}')
+print(f'KNN test score:  {knn.score(X_test,y_test)}')
+print(confusion_matrix(y_test, knn.predict(X_test)))
+print(classification_report(y_test, knn.predict(X_test)))
+
+
+#%% DecisionTreeClassifier()
+dtree = DecisionTreeClassifier(max_depth=5, random_state=1)
+# Fit dt to the training set
+dtree.fit(X_train,y_train)
+# Predict test set labels
+y_test_pred = dtree.predict(X_test)
+# Evaluate test-set accuracy
+print(accuracy_score(y_test, y_test_pred))
+print(confusion_matrix(y_test, y_test_pred))
+print(classification_report(y_test, y_test_pred))
 
 # %%
 
